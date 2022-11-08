ed0bf8bfe12b715a97470c5e12004d31c44463a7
salihgun <salihgun97@outlook.com>
2022-11-08 22:17:43 +0300

    feat: example added

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,34 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-video-processor';
+import { StyleSheet, View, Text, Pressable } from 'react-native';
+import { getVideoInfo, VideoInfoType } from 'react-native-video-processor';
+import ImagePicker from 'react-native-image-crop-picker';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [videoInfo, setVideoInfo] = React.useState<VideoInfoType>({
+    duration: 0,
+    creationDate: '',
+  });
 
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+  const onPickVideo = async () => {
+    const video = await ImagePicker.openPicker({
+      mediaType: 'video',
+    });
+    const videoInfoResponse = await getVideoInfo(video.path);
+    setVideoInfo(videoInfoResponse);
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Pressable style={styles.buttonContainer} onPress={onPickVideo}>
+        <Text>Choose Video</Text>
+      </Pressable>
+      <View>
+        <Text style={styles.text}>
+          Duration: {videoInfo.duration.toFixed(2)} seconds
+        </Text>
+        <Text style={styles.text}>Creation Date: {videoInfo.creationDate}</Text>
+      </View>
     </View>
   );
 }
@@ -23,9 +39,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+  buttonContainer: {
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 20,
+  },
+  text: {
+    color: 'black',
+    marginBottom: 5,
   },
 });
